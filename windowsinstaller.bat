4d31037baa4e843774a6fea3bf43bd06b7072e25
Nightmaregodss <81895400+Nightmaregodss@users.noreply.github.com>
2022-02-19 21:00:00 +0530

    Update windowsinstaller.bat

--- windowsinstaller.bat
+++ windowsinstaller.bat
@@ -1,1 +1,10 @@
 
+echo qwakeactyl windows installer | not as good as lynix one but we will try to upgrade it always
+
+git clone https://github.com/Qwakeactyl/Qwakeactyl
+cd Qwakeactyl
+npm install
+
+echo done you will if you need any help in configuring qwakeactyl join our discord https://discord.gg/eBBKeuB7p2
+
+echo and you will have to do settings.json
